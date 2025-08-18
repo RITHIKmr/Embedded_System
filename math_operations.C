44719bb325937c25eef6ffd1dada914c000c7a88
rithik <mrrithik2000@gmail.com>
2025-08-18 15:32:52 +0530

    implemented scanf with functions

--- math_operations.C
+++ math_operations.C
@@ -6,13 +6,19 @@ int mul(int,int);
 int div(int,int);
 
 int main(){
-    int a,b,ans;
+    int a,b;
     printf("enter a number");
     scanf("%d",&a);
     printf("enter another number");
     scanf("%d",&b);
-    ans=a+b;
-    printf("the answer is%d",ans);
+    int add_res=add(a,b);
+    int sub_res=sub(a,b);
+    int mul_res=mul(a,b);
+    int div_res=div(a,b);
+    printf("the sum is=%d\n",add_res);
+    printf("the substraction result is=%d\n",sub_res);
+    printf("the multiplication result is=%d\n",mul_res);
+    printf("the division result is=%d\n",div_res);
 }
 //function definition
 int add(int a,int b){
